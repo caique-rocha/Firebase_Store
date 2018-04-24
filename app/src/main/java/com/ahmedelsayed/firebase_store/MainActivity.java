1559efa07110a5ef3c2f4e13c8fc6a67aa0a355c
Ahmed Elsayed Ahmed <37459585+AhmedElsayed94@users.noreply.github.com>
2018-04-24 18:09:40 +0200

    Initial commit

---
+++ app/src/main/java/com/ahmedelsayed/firebase_store/MainActivity.java
@@ -0,0 +1,78 @@
+package com.ahmedelsayed.firebase_store;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextInputEditText password,email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email=(TextInputEditText)findViewById(R.id.login_email);
+        password=(TextInputEditText)findViewById(R.id.login_password);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null)
+        {
+            Intent home=new Intent(MainActivity.this,HomeActivity.class);
+            startActivity(home);
+            finish();
+        }
+
+    }
+
+    public void openRegister(View view) {
+        Intent register =new Intent (MainActivity.this , RegisterActivity.class);
+        startActivity(register);
+        finish();
+    }
+
+    public void Login(View view) {
+        String passwordText,emailText;
+
+
+        passwordText=password.getText().toString();
+        emailText=email.getText().toString();
+
+
+
+        mAuth.signInWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent home=new Intent(MainActivity.this,HomeActivity.class);
+                            startActivity(home);
+                            finish();
+
+                        } else {
+                            Log.e("my_store", "failure", task.getException());
+
+                        }
+
+                        // ...
+                    }
+                });
+
+
+    }
+
+}
---
+++ app/src/main/java/com/ahmedelsayed/firebase_store/RegisterActivity.java
@@ -0,0 +1,99 @@
+package com.ahmedelsayed.firebase_store;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    TextInputEditText email,password,confirmPassword;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email=(TextInputEditText)findViewById(R.id.register_email);
+        password=(TextInputEditText)findViewById(R.id.register_password);
+        confirmPassword=(TextInputEditText)findViewById(R.id.register_confirm_password);
+
+
+    }
+
+    public void openLogin(View view) {
+        Intent login =new Intent (this , MainActivity.class);
+        startActivity(login);
+    }
+
+    public void Register(View view) {
+        String emailText,passwordText,confirmPasswordText;
+
+        emailText=email.getText().toString();
+        passwordText=password.getText().toString();
+        confirmPasswordText=confirmPassword.getText().toString();
+
+        if(emailText.isEmpty()||emailText.equals(" ") || !emailText.matches(emailPattern))
+        {
+            email.setError("Invalid Email !");
+            return;
+        }
+
+        if(passwordText.isEmpty()||passwordText.equals(" "))
+        {
+            password.setError("Invalid Password  !");
+            return;
+        }
+
+        if(confirmPasswordText.isEmpty()||confirmPasswordText.equals(" "))
+        {
+            confirmPassword.setError("Invalid Password  !");
+            return;
+        }
+
+        if(passwordText.length()<8)
+        {
+            password.setError("Length must be greater than 8");
+            return;
+        }
+
+
+        if(!confirmPasswordText.equals(passwordText))
+        {
+            confirmPassword.setError("Password not match !");
+            return;
+        }
+
+
+
+
+       mAuth.createUserWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent home=new Intent(RegisterActivity.this,HomeActivity.class);
+                            startActivity(home);
+                            finish();
+
+                        } else {
+
+                            Log.e("my_store", "failure", task.getException());
+                        }
+                    }
+                });
+
+    }
+
+}
